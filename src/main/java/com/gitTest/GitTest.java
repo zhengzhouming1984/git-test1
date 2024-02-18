ac1efeedbc41c474e24ffed4ce6ec8ef0cb7ab93
zhengzhouming <94711123@qq.com>
2024-02-18 23:05:34 +0800

    hot-fix second commit

--- src/main/java/com/gitTest/GitTest.java
+++ src/main/java/com/gitTest/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("gitTest3");
         System.out.println("gitTest4");
         System.out.println("gitTest5");
+        System.out.println("hot-fix gitTest5");
     }
 }
