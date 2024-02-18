72f1422ab01310e9ac154aa4e5b1b5ccb72d4d0b
zhengzhouming <94711123@qq.com>
2024-02-18 21:30:59 +0800

    third commit

--- src/main/java/com/gitTest/GitTest.java
+++ src/main/java/com/gitTest/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("gitTest");
         System.out.println("gitTest1");
+        System.out.println("gitTest2");
     }
 }
